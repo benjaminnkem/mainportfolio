677d7f398b8b54bf132617db6a38ffcb011d36eb
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-02-05 19:17:25 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="main-title">N. Benjamin</h1>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">Contact</a>
+        <a href="/">About</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
